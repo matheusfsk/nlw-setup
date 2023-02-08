12758db869f56bc99b6027b9e655945404cb2102
Matheus Fonseca <teus.fonseca@hotmail.com>
2023-02-08 20:22:32 -0300

    Criação de interface de cadastros mobile, habitos do dia, componentes, checkbox e progressbar

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import { View, Text, ScrollView } from "react-native";
 
 import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
@@ -6,11 +7,14 @@ const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
 
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
+
 const datesFromYearStart = generateDatesFromYearBeginning();
 const minimumSummaryDatesSizes = 18 * 5;
 const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length;
 
 export function Home() {
+  const { navigate } = useNavigation();
+
   return (
     <View className="flex-1 bg-background px-8 py-16">
       <Header />
@@ -33,7 +37,10 @@ export function Home() {
       >
         <View className="flex-row flex-wrap">
           {datesFromYearStart.map((date) => (
-            <HabitDay key={date.toISOString()} />
+            <HabitDay
+              key={date.toISOString()}
+              onPress={() => navigate("habit", { date: date.toISOString() })}
+            />
           ))}
 
           {amountOfDaysToFill > 0 &&
