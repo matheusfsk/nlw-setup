999f20de964e636c52c8403c9d3a5dc320154eec
Matheus Fonseca <teus.fonseca@hotmail.com>
2023-02-13 19:55:32 -0300

    buscando resumos da API

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -4,19 +4,21 @@ import { ProgressBar } from "./ProgressBar";
 import * as Checkbox from "@radix-ui/react-checkbox";
 import { Check } from "phosphor-react";
 interface HabitDayProps {
-  completed: number;
-  amount: number;
+  date: Date;
+  completed?: number;
+  amount?: number;
 }
 
-export function HabitDay({ completed, amount }: HabitDayProps) {
-  const completedPercentage = Math.round((completed / amount) * 100);
+export function HabitDay({ completed = 0, amount = 0 }: HabitDayProps) {
+  const completedPercentage =
+    amount > 0 ? Math.round((completed / amount) * 100) : 0;
   return (
     <Popover.Root>
       <Popover.Trigger
         className={clsx("w-10 h-10 rounded-lg border-2", {
           " bg-zinc-900 border-zinc-800": completedPercentage === 0,
           "bg-violet-900 border-violet-700":
-            completedPercentage >= 0 && completedPercentage < 20,
+            completedPercentage > 0 && completedPercentage < 20,
           "bg-violet-800 border-violet-600":
             completedPercentage >= 20 && completedPercentage < 40,
           "bg-violet-700 border-violet-500":
